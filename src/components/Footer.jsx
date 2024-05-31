45f17392b64700886a0ce718a0ff556ed7f97e2a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-05-30 19:47:23 -0600

    Adding some information to the footer like mail and cel

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer>
             <div className="contact-container">
                 <h3>Contacto</h3>
-                <p>Correo: ejemplo@mail.com</p>
-                <p>Teléfono: 123456789</p>
-                <p>Dirección: Calle Ejemplo, Ciudad</p>
+                <p>Correo: recuerdosdeabuelahillo@gmail.com</p>
+                <p>Teléfono: +52 662 469 8604</p>
+                <p>Hermosillo - Sonora, México</p>
             </div>
             <div className="social-container">
                 <h3>Redes Sociales</h3>
