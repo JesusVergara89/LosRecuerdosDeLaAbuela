a311729e278d9282c379c6cce9ddab9426ff2726
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-06-12 12:46:44 -0700

    new contact number

--- src/components/Basket.jsx
+++ src/components/Basket.jsx
@@ -52,7 +52,7 @@ const Basket = () => {
     }).join('\n');
 
     const sendMessageToWhatsApp = () => {
-        const phoneNumber = '+573222117823';
+        const phoneNumber = '+526624698604';
         const message = `Hola estoy interesad@ en los siguientes productos:\n----\n${productInfo}\n\nEl valor a pagar es: ${totalValues.reduce((accumulator, currentValue) => accumulator + currentValue, 0)}`;
         const whatsappLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
         window.open(whatsappLink);
